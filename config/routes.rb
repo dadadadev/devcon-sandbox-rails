d06b54e3c2c1d5efebbd25d25e07d1d2d66ec7c4
dadadadev <dadada80.dev@gmail.com>
2023-04-24 15:44:36 +0000

    create Books controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'home/index'
   get '/help', to: 'home#help'
+  get '/books', to: 'books#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
