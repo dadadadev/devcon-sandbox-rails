596c93c5757e9967865781df95774a3afaf2959f
dadadadev <dadada80.dev@gmail.com>
2023-04-27 12:13:28 +0000

    refacter

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,11 @@
 class HomeController < ApplicationController
   def index
     @message = "This is a test site of Ruby on Rails."
-    @links = [ "users", "books", "help" ]
+    @menus = [
+      { :label => "User", :path => users_path },
+      { :label => "Book", :path => books_path },
+      { :label => "Help", :path => help_path },
+    ]
   end
 
   def help
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
-  resources :users
   root 'home#index'
   get 'home/index'
   get '/help', to: 'home#help'
-  get '/books', to: 'books#index'
-  get '/books/new', to: 'books#new', as: :new_book
-  post '/books', to: 'books#create'
-  get '/books/:id', to: 'books#show', as: :book
-  get 'books/:id/edit', to: 'books#edit', as: :edit_book
-  patch '/books/:id', to: 'books#update'
-  delete '/books/:id', to: 'books#destroy'
+  resources :users
+  resources :books
+  # get '/books', to: 'books#index'
+  # get '/books/new', to: 'books#new', as: :new_book
+  # post '/books', to: 'books#create'
+  # get '/books/:id', to: 'books#show', as: :book
+  # get 'books/:id/edit', to: 'books#edit', as: :edit_book
+  # patch '/books/:id', to: 'books#update'
+  # delete '/books/:id', to: 'books#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
