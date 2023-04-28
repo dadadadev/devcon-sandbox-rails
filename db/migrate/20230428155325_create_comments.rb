74b4e95a551837c77bf9c6889632c2ce66b1a88e
dadadadev <dadada80.dev@gmail.com>
2023-04-28 16:04:23 +0000

    fix db

--- db/migrate/20230428155325_create_comments.rb
+++ db/migrate/20230428155325_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
       t.string :commenter
-      t.text :bod
+      t.text :body
       t.references :book, null: false, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_23_164318) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_28_155325) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "book_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_comments_on_book_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -25,4 +34,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_23_164318) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "books"
 end
